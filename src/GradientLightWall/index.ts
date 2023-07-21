8fc23ebc82345c444610569c0eb2403c89e20af8
JYuan <31184164+WaterSeeding@users.noreply.github.com>
2023-07-21 15:01:56 +0800

    see: 检测动态渐变墙

--- src/GradientLightWall/index.ts
+++ src/GradientLightWall/index.ts
@@ -3,29 +3,18 @@ import { Graphics } from "./Graphics";
 import * as Cesium from "cesium";
 import { viewer } from "../main";
 
-const clone = (children: any, cloneTarget: any) => {
-  let keys = Object.keys(cloneTarget.prototype);
+const clone = (extendObj: any, baseObj: any) => {
+  let keys = Object.keys(baseObj.prototype);
   for (let i = 0, len = keys.length; i < len; i++) {
-    children.prototype[keys[i]] = cloneTarget.prototype[keys[i]];
+    extendObj.prototype[keys[i]] = baseObj.prototype[keys[i]];
   }
 };
 
 const prototypeExtends = (viewer: Cesium.Viewer) => {
-  return (parents: any, children: any) => {
+  return (baseObj: any, extendObj: any) => {
     let obj: any = null;
-    if (Array.isArray(parents)) {
-      parents.forEach((p) => {
-        clone(children, p);
-      });
-      obj = new children(viewer);
-      parents.forEach((pp) => {
-        pp.call(obj, viewer);
-      });
-    } else {
-      clone(children, parents);
-      obj = new children(viewer);
-      parents.call(obj, viewer);
-    }
+    clone(extendObj, baseObj);
+    obj = new extendObj(viewer);
     return obj;
   };
 };
